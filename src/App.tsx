def4cd3aaccf2f9b7bdec4b9c05718b64cf56516
archil-qb <archil-nu@outlook.com>
2024-09-07 14:03:17 -0400

    Adding react template for typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <header>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </React.Fragment>
+  );
+}
+
+export default App;
