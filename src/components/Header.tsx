eaf9255fc51ff6551e25e9faa863b5450705d31a
archil-nu@outlook.com <a.lelashvili@northeastern.edu>
2024-09-08 18:48:23 -0400

    Adding skeleton.css

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type HeaderProps = {
+    navLinks: {
+        title: string,
+        href: string,
+    }[]
+}
+
+const Header = ({ navLinks }: HeaderProps) => {
+    return null;
+}
+
+export default Header;
