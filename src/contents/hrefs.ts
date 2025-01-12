3da1fadff2c43709b7382b8107dbc7cac32a613e
Archil Lelashvili <archil-nu@outlook.com>
2025-01-12 14:25:30 -0500

    Updated resume link

--- src/contents/hrefs.ts
+++ src/contents/hrefs.ts
@@ -1,7 +1,7 @@
 const hrefs = {
     github: 'https://github.com/archil-l',
     linkedin: 'https://www.linkedin.com/in/archil-nu/',
-    resume: 'https://1drv.ms/b/s!AuJuRAdeWsEhiq4iJx0hAS79rnre1A',
+    resume: 'https://1drv.ms/b/c/21c15a5e07446ee2/EeJuRAdeWsEggCEilwIAAAABa9B95fj4EiAnRxn3v3Y04w?e=Oa3sdC',
     newNav: 'https://helpv2.quickbase.com/hc/en-us/articles/30212936236948-Quickbase-September-2024-Release-Notes#h_01J7BD3PQ33X5HVGPX3SEJSCM8',
     newSummaryReport: 'https://helpv2.quickbase.com/hc/en-us/articles/23804944584468-Quickbase-February-2024-Release-Notes#h_01HPT7ASHWVSMB064ADPVMTR80',
     newFormsExperience: 'https://helpv2.quickbase.com/hc/en-us/articles/16259648646292-Quickbase-June-2023-release-notes#h_01GAH1YHWP30JQ63J1ZCZ74YCN',
