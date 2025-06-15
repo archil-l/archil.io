d7b88acc46b8316db9c0d929db5414adfbf88d1a
archil-l <archil.lelashvili@gmail.com>
2025-06-15 16:27:29 -0400

    Changing file naming convention - rest of the files

---
+++ src/components/nav/messages.ts
@@ -0,0 +1,18 @@
+import { defineMessages } from 'react-intl';
+
+const navMessages = defineMessages({
+  welcome: {
+    id: 'nav.welcome',
+    defaultMessage: 'Home',
+  },
+  work: {
+    id: 'nav.work',
+    defaultMessage: 'Work',
+  },
+  projects: {
+    id: 'nav.projects',
+    defaultMessage: 'Projects',
+  },
+});
+
+export default navMessages;
