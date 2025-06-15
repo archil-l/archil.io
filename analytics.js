649da93b7bda4deb850773db886f26ca8f4989a8
archil-l <archil.lelashvili@gmail.com>
2025-06-15 16:29:27 -0400

    Updates

--- analytics.js
+++ analytics.js
@@ -1,5 +1,7 @@
 window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
+function gtag() {
+  window.dataLayer.push(arguments);
+}
 gtag('js', new Date());
 
 gtag('config', 'G-F7CFY916YT');
