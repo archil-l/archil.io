b56023cb4d745d87a5c11f407a0930959c4b1d99
archil-l <archil.lelashvili@gmail.com>
2025-07-18 22:25:35 -0400

    Updating resume link

--- src/content/externalLinks.ts
+++ src/content/externalLinks.ts
@@ -5,6 +5,5 @@
 export const socialLinks = {
   github: 'https://github.com/archil-l',
   linkedin: 'https://www.linkedin.com/in/archil-l/',
-  resume:
-    'https://1drv.ms/b/c/21c15a5e07446ee2/EeJuRAdeWsEggCEilwIAAAABa9B95fj4EiAnRxn3v3Y04w?e=Oa3sdC',
+  resume: 'https://1drv.ms/b/c/21c15a5e07446ee2/EUFsXnrsGAhPtOYfzk-WwkgBq_HwdXPCTG5MY7_GA8wC_w',
 };
